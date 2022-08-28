0fb749c5c682aa5d326bed6eaae05ef8456a1846
Forrest Porter <5495476+fporter@users.noreply.github.com>
2022-08-27 19:23:50 -0500

    :sparkles: working dynamic resolutions
    Allows different assets to be loaded at runtime.
    This is mainly to not have large images used for smaller screens.
    Bringing the total package size up though.
    Not commiting any assets, but they are just godot projects.
    Only thing changed, is the export template for linux was added.
    
    Ref #6

--- src/Scripts/Levels/MainMenu.gd
+++ src/Scripts/Levels/MainMenu.gd
@@ -1,15 +1,44 @@
-extends Node2D
+extends Control
 
 
 # Declare member variables here.
 onready var quit_button = get_node("Panel/MarginContainer/CenterContainer/PanelContainer/VBoxContainer/QuitBtn")
+# for development only, use test sizes
+var projectResolution=Vector2(ProjectSettings.get_setting("display/window/size/test_width"),ProjectSettings.get_setting("display/window/size/test_height"))
+var loadedPck
 
+func _init():
+	
+	var targetWidth
+	if OS.has_feature("standalone"):
+		targetWidth = OS.get_screen_size()[0]
+	else:
+		targetWidth = ProjectSettings.get_setting("display/window/size/test_width")
+	
+	var availableResolutions = [1280,1920,3840]
+	var selectedWidth = 99999
+	for width in availableResolutions:
+		if width >= targetWidth:
+			selectedWidth = width
+			break
+			
+	loadedPck = ProjectSettings.load_resource_pack("res://res/" + str(selectedWidth) + ".pck")
+	
+	# we can change settings and reload the scene to change asset pcks
+	#ProjectSettings.set_setting("display/window/size/test_width",1280)
+	#ProjectSettings.set_setting("display/window/size/test_height",720)
+	#get_tree().reload_current_scene()
+	
+	print("Selected width: " + str(selectedWidth))
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if loadedPck:
+		# we must use ResourceLoader to pass 'true' to the no-cache option
+		$Panel.get_stylebox("panel","").set_texture(ResourceLoader.load("res://assets/bgs/MainScreen.png", "", true))
+		print("should use new image")
 	quit_button.connect("button_up", self, "on_quit")
 	$Panel/MarginContainer/CenterContainer/PanelContainer/VBoxContainer/PlayBtn.grab_focus()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,7 +46,7 @@ func _ready():
 #	pass
 func _input(event):
 	if(event.is_action_pressed("ui_cancel")):
-		get_tree().quit()
+		on_quit()
 		
 func on_quit():
 	get_tree().quit()
